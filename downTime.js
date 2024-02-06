03c7112ec8c88161c19dd053d8f1b50371d85d8f
Avindra Singh <avindra562@gmail.com>
2024-02-06 18:41:50 +0530

    message

---
+++ downTime.js
@@ -0,0 +1,9 @@
+function populate_server(){
+    var input = document.getElementById('server');
+    input.value = 'Hello...';
+}
+
+function populate_app(){
+    var input = document.getElementById('app');
+    input.value = 'Hello...';
+}
